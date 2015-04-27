a0c1c855c4b804069b383f4f48339649f348709e
rettknight <rodolfosilva@me.com>
2015-04-27 09:47:44 -0500

    Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,23 +2,28 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
-	attr_accessor :name, :email #getters & setters
-	attr_accessible :name, :email
+	attr_accessor :password 
+	attr_accessible :name, :email, :password, :password_confirmation
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	# .permit(:name, :email)
 	validates :name, :presence => true, #has to be inserted
-					 :length   => { :maximum = 50 } 
+					 :length   => {:maximum => 50} 
 
 	validates :email, :presence => true, 
 					  :format   => {:with => email_regex},
 					  :uniqueness => {:case_sensitive => false}
+
+	validates :password, :presence => true,
+						 :confirmation => true,
+						 :length => {:within => 6..30} 
 end
