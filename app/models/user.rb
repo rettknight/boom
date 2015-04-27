6d64c3f56ad16d7a5d14c065f6bf0306cc52b996
rettknight <rodolfosilva@me.com>
2015-04-27 11:15:49 -0500

    Users CSS

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,9 @@
 
 class User < ActiveRecord::Base
 	attr_accessor :password 
-	attr_accessible :name, :email, :password, :password_confirmation
-
+	# attr_accessible :name, :email, :password, :password_confirmation 
+	# attr_accessible required? 
+	
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	# .permit(:name, :email)
 	validates :name, :presence => true, #has to be inserted
