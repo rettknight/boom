d4d18da3dd670f7f2b72d5ed807a7753639b3af6
rettknight <rodolfosilva@me.com>
2015-04-27 16:15:16 -0500

    Finished User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,5 +21,6 @@ class UsersController < ApplicationController
    end 
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    #require :user - important, allows it to be created
   end
 end
