b47844db3b63abaebdb83099d71d32a1f50a5112
rettknight <rodolfosilva@me.com>
2015-04-27 17:09:31 -0500

    Sessions controller y new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
-  get 'users/new'
   get 'pages/home' 
   get '/signup', :to => 'users#new'
+  get '/signin', :to => 'sessions#new'
+  get '/signout', :to => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
