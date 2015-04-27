6b12e6d6c1705c64e85b5eb49fb92ff5274c32f4
rettknight <rodolfosilva@me.com>
2015-04-27 17:30:37 -0500

    Falla signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
   	@title = "Sign in"
   end
   def create
-  	
+  	user = User.authenticate(params[:session][:email],
+  							 params[:session][:password])
+  	if user.nil?
+  		flash.now[:error] = "Invalid email/password."
+  		@title = "Sign in"
+  		render 'new'
+  	else	
+  		#Success
+  	end
   end
   def destroy
   	
+  end
+
+  def session_params
+  	session_params.require(:session).permit(:email,:password)
   end
 end
