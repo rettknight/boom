69311dcaa2fcfcc9a6bf1eacbbd2e29e655a2c48
rettknight <rodolfosilva@me.com>
2015-04-27 15:49:38 -0500

    CSS, User Signup, Etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,19 @@ class UsersController < ApplicationController
 
   def new
    @user = User.new
-   @title = "- Sign Up!"
+   @title = "- Sign up"
   end
 
   def create
    	@user = User.new(user_params)
-	if @user.save
-		#Success
-	else
-		render 'new'
-	end
-  end
-
+		if @user.save
+			redirect_to user_path(@user), :flash => {:success => "User created."}
+		else
+			@title = "- Sign up "
+			render 'new'
+		end
+   end 
   def user_params
-  	params.permit(:name, :email, :password, :password_confirmation)
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
 	
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	# .permit(:name, :email)
-	validates :name, :presence => true, #has to be inserted
+	validates :name, :presence => true, 
 					 :length   => {:maximum => 50} 
 
 	validates :email, :presence => true, 
