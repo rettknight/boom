23d8c05c36316538ceb75c596d69172b7f502977
rettknight <rodolfosilva@me.com>
2015-04-27 13:33:16 -0500

    Users form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
    @title = "- Sign Up!"
   end
 
+  def create
+   	@user = User.new(user_params)
+	if @user.save
+		#Success
+	else
+		render 'new'
+	end
+  end
+
+  def user_params
+  	params.permit(:name, :email, :password, :password_confirmation)
+  end
 end
